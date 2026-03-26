688e7990976f2a6b2a5be5cb39004e35c53aa3e8
Jan Avkovsky <avkovjan@fjfi.cvut.cz>
2026-03-26 17:02:52 +0100

    plot-ea.ipynb: I included the analysis of the smeared data tree in both multiplicity and S0pT1. I also added a multiplicity response matrix.
    pythia8_generate-tree.ipynb:
    I now include the mass of the particles in the events.root.
    I added a new section for simulation of detector effects using RDataFrame. I wrote simple simulations for acceptance of TPC and TOF and for smearing pT, eta, phi utilizing the PtEtaPhiMVector. The efficiency and smearing functions are written in C++ and loaded via the ROOT's interpreter.

--- cpp/S0pT1.cpp
+++ cpp/S0pT1.cpp
@@ -1,10 +1,8 @@
 // import libraries
-#include <cmath> // standard math functions
-#include <vector> // dynamic lists
-#include "TMath.h" // ROOT's TMath
-#include "ROOT/RVec.hxx" // ROOT's fast array of numbers
+#include <TMath.h> // ROOT's TMath
+#include <ROOT/RVec.hxx> // ROOT's fast array of numbers
 
-// define the function to calculate unweighted transverse spherocity
+// define the function to calculate unweighted transverse spherocity for an event
 double calc_S0pT1(const ROOT::RVec<float>& uxEvent, // const is a safety lock to ensure that the function only reads data and does not change it
                   const ROOT::RVec<float>& uyEvent, // & tells C++ to use the original data rather than making copies in RAM
                   int NchEvent) {
@@ -16,7 +14,7 @@ double calc_S0pT1(const ROOT::RVec<float>& uxEvent, // const is a safety lock to
 
         // loop over particles
         for (size_t j = 0; j < NchEvent; j++) {
-            sumCrossProducts += std::abs(uxEvent[j]*uyEvent[i] - uyEvent[j]*uxEvent[i]); // calculate sums of absolute values of cross products
+            sumCrossProducts += TMath::Abs(uxEvent[j]*uyEvent[i] - uyEvent[j]*uxEvent[i]); // calculate sums of absolute values of cross products
         }
 
         // overwrite minimum value if we find a lower sum
@@ -26,5 +24,5 @@ double calc_S0pT1(const ROOT::RVec<float>& uxEvent, // const is a safety lock to
     }
 
     // return S0pT1
-    return (TMath::Pi() * TMath::Pi() / 4.0) * std::pow(minVal / NchEvent, 2);
+    return (TMath::Pi() * TMath::Pi() / 4.0) * TMath::Power(minVal / NchEvent, 2);
 }
